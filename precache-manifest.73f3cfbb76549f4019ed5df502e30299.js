4b3e87170a6bb5b062dfe9605317265c1baa4ff1
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2019-10-13 03:11:31 -0300

    Updates

---
+++ precache-manifest.73f3cfbb76549f4019ed5df502e30299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93b1156833d659b143cbcc8df0c4733",
+    "url": "/Pokemon-card-game/index.html"
+  },
+  {
+    "revision": "d9f5c179a6d2ce4a75c9",
+    "url": "/Pokemon-card-game/static/css/main.15274219.chunk.css"
+  },
+  {
+    "revision": "20191da8d7be18061668",
+    "url": "/Pokemon-card-game/static/js/2.afbde453.chunk.js"
+  },
+  {
+    "revision": "d9f5c179a6d2ce4a75c9",
+    "url": "/Pokemon-card-game/static/js/main.735bf3f2.chunk.js"
+  },
+  {
+    "revision": "80c47209b8ca53d8eee9",
+    "url": "/Pokemon-card-game/static/js/runtime-main.76598652.js"
+  }
+]);
